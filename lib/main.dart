6a9290783038c56ae855bc0fe47bd24ada3285f8
Deepak bharti <deepakbhartiad@gmail.com>
2023-10-30 15:28:13 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sample/pages/welcome_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Shortcuts(
+        shortcuts: <LogicalKeySet, Intent>{
+          LogicalKeySet(LogicalKeyboardKey.select): const ActivateIntent(),
+        },
+        child: MaterialApp(
+          title: 'Better player demo',
+          // localizationsDelegates: [
+          //   GlobalMaterialLocalizations.delegate,
+          //   GlobalWidgetsLocalizations.delegate,
+          // ],
+          supportedLocales: [
+            const Locale('en', 'US'),
+            const Locale('pl', 'PL'),
+          ],
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+          ),
+          home: WelcomePage(),
+        ));
+  }
+}
